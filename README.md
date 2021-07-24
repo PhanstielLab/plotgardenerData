71b1341e38f5bb04a19d8f9d13ba9ddd38874a5c
Nicole Kramer <nekramer@live.unc.edu>
2021-07-24 18:38:19 -0400

    removed old data
    Former-commit-id: 5bc5391cae9ebe366eda6e1f0e87fe7a6c3951e0 [formerly f4c24a40314b23861f0be35c4bcd43d9cf384158]
    Former-commit-id: ffb3b73cdaf8835c890b7aab58387f8f9b9972a5

---
+++ README.md
@@ -0,0 +1,6 @@
+# BentoBoxData
+
+This package provides supplemental datasets and files for use with 
+[BentoBox](https://github.com/PhanstielLab/BentoBoxData). For more
+information on how to use these data, please refer to the 
+`BentoBox` vignette.
---
+++ inst/script/exampleFiles.R
@@ -0,0 +1,8 @@
+# test.bw from rtracklayer
+# test_chr22.hic made with Juicer Tools pre: 
+#           https://github.com/aidenlab/juicer/wiki/Pre
+# Input file found here: 
+#           https://bcm.app.box.com/v/juicer-tools-testing
+# Command:
+#           java -jar juicer_tools_1.22.01.jar pre -r 2500000,100000 -c 22 \
+#           -v -d test.txt.gz test_chr22.hic hg19
